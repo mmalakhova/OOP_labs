dd563daf962f82a723de538769349da847381c1c
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2020-12-04 05:03:32 +0700

    Update parser.cpp

--- Game_of_life/parser.cpp
+++ Game_of_life/parser.cpp
@@ -1,6 +1,4 @@
 #include "parser.h"
-#include "console.h"
-#include "game_board.h"
 
 parser::parser(game_board* g) {
     game = g;
@@ -12,22 +10,38 @@ void parser::get_comand(console &c) {
 
     if (comand_opt == "reset"){
         game->reset();
+        game->show_board();
+        game->show_step_count();
     } else if (comand_opt == "clear"){
-        game->clear();
+        string arg;
+        arg = c.get_com_arguments();
+        size_t x = arg[0] - '0' - 17;
+        size_t y = arg[1] - '0';
+        game->clear(x,y);
+        game->show_board();
+        game->show_step_count();
     } else if (comand_opt == "set"){
         string arg;
         arg = c.get_com_arguments();
         size_t x = arg[0] - '0' - 17;
         size_t y = arg[1] - '0';
         game->add_live_cell(x,y);
+        game->show_board();
+        game->show_step_count();
     } else if (comand_opt == "step"){
         game->next_state();
+        game->show_board();
+        game->show_step_count();
     } else if (comand_opt == "back"){
         game->back();
+        game->show_board();
+        game->show_step_count();
     } else if (comand_opt == "save"){
         game->save_board();
+        game->show_board();
     } else if (comand_opt == "load"){
         game->load_board();
+        game->show_board();
     }
 }
 
