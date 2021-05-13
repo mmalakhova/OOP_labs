3c994a60700f06acfacbea979c7821657c8eba21
mmalakhova <m.malakhova@g.nsu.ru>
2021-05-13 13:26:10 +0700

    fixing main

--- Java_labs/lab2/src/ru/nsu/ccfit/malakhova/ClassIdentifier.java
+++ Java_labs/lab2/src/ru/nsu/ccfit/malakhova/ClassIdentifier.java
@@ -8,19 +8,10 @@ public class ClassIdentifier {
         Properties prop = new Properties();
         String path = null;
 
-//        try(InputStream stream = new FileInputStream("comm.config")) {
-//            prop.load(stream);
-//
-//            for (String str : prop.stringPropertyNames()) {
-//                if (str.equals(name)) {
-//                    path = prop.getProperty(str);
-//                    break;
-//                }
-//            }
-//        }
-        try(InputStream inp = ClassLoader.getSystemResourceAsStream("comm.config"))
+        try(InputStream inp = ClassIdentifier.class.getResourceAsStream("comm.config"))
         {
-            if (inp == null) throw new IOException("Problems with config");
+            if (inp == null)
+                throw new IOException("Problems with config");
             prop.load(inp);
             for (String str : prop.stringPropertyNames()) {
                 if (str.equals(name)) {
